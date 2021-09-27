b90be12a7b9c1e8c0fbd3cfac0dd5336e9a32d38
Lydie Costes <lvc3@duke.edu>
2021-09-27 16:56:50 -0400

    Task 3 - tracking tool update

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,38 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Lydie Costes(lydie.costes@duke.edu)
+# Date:   Fall 2021
+#--------------------------------------------------------------
+
+#Create a variable pointing to the data file
+fileName = './data/raw/sara.txt'
+
+#Create a file object from the file
+fileObj = open(fileName,'r')
+
+#Read contents of file into a list
+lineList = fileObj.readlines()
+
+#Close the file
+fileObj.close()
+
+#Pretend we read one line of data from the file
+lineString = lineList[100]
+
+# Use the split command to parse the items in lineString into a list object
+lineData = lineString.split()
+
+# Assign variables to specfic items in the list
+record_id = lineData[0]   # ARGOS tracking record ID
+obs_date = lineData[2]   # Observation date
+ob_lc = lineData[4]       # Observation Location Class
+obs_lat = lineData[6]     # Observation Latitude
+obs_lon = lineData[7]     # Observation Longitude
+
+# Print information to the use
+print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
